f6bcecc5c810aaeba68d9873d429ffc0677103e3
Andrew Cain <acain@swin.edu.au>
2013-10-05 16:29:06 +1000

    Move to multiple bugs to splat

--- BugSplat/src/GameMain.pas
+++ BugSplat/src/GameMain.pas
@@ -3,7 +3,7 @@ uses SwinGame, sgTypes;
 
 type
   BuggyGameData = record
-    buggy: Sprite;
+    bugs: array of Sprite;
   end;
 
 procedure LoadResources();
@@ -12,6 +12,8 @@ begin
 end;
 
 procedure PlaceBuggy(s: Sprite);
+var
+  target: Point2D;
 begin
   // Place either top/bottom 50% or left/right 50%
   if Rnd(2) = 0 then // place top/bottom
@@ -34,15 +36,25 @@ begin
   end;
 
   // Move toward a random screen point
-  SpriteSetVelocity(s, VectorMultiply(UnitVector(VectorFromPoints(CenterPoint(s), RandomScreenPoint())), 2));
+  target := PointAt(ScreenWidth() * 0.1 + Rnd(Round(ScreenWidth() * 0.8)), ScreenHeight() * 0.1 + Rnd(Round(ScreenHeight() * 0.8)));
+  SpriteSetVelocity(s, VectorMultiply(UnitVector(VectorFromPoints(CenterPoint(s), target)), 2));
   SpriteStartAnimation(s, 'fly');
+  SpriteSetValue(s, 'Alive', 1);
+  SpriteSetValue(s, 'Gone', 0);
+end;
+
+procedure CreateBug(var buggy: Sprite);
+begin
+  buggy := CreateSprite(BitmapNamed('Buggy'), AnimationScriptNamed('BuggyAnimations'));
+  SpriteAddValue(buggy, 'Alive', 1);
+  SpriteAddValue(buggy, 'Gone', 0);
+  PlaceBuggy(buggy);
 end;
 
 procedure SetupGameData(var game: BuggyGameData);
 begin
-  game.buggy := CreateSprite(BitmapNamed('Buggy'), AnimationScriptNamed('BuggyAnimations'));
-
-  PlaceBuggy(game.buggy);
+  SetLength(game.bugs, 1);
+  CreateBug(game.bugs[0]);
 end;
 
 procedure KeepOnScreen(s: Sprite);
@@ -58,34 +70,100 @@ begin
   end;
 end;
 
-procedure HandleInput(var game: BuggyGameData);
+procedure CheckHitBug(var game: BuggyGameData);
 var
-  pt: Point2D;
+  i: Integer;
+begin
+  for i := 0 to High(game.bugs) do
+  begin
+    if (SpriteValue(game.bugs[i], 'Alive') = 1) and SpriteRectCollision(game.bugs[i], MouseX() - 20, MouseY() - 20, 40, 40) then
+    begin
+      SpriteStartAnimation(game.bugs[i], 'splat');
+      SpriteSetValue(game.bugs[i], 'Alive', 0);
+      exit;
+    end;
+  end;
+
+  PlaySoundEffect('Hit');
+end;
+
+procedure HandleInput(var game: BuggyGameData);
 begin
   if MouseClicked(LeftButton) then
   begin
-    if SpriteRectCollision(game.buggy, MouseX() - 10, MouseY() - 10, 20, 20) then
+    CheckHitBug(game);
+  end;
+end;
+
+procedure BounceBugs(var bugs: array of Sprite);
+var
+  i, j: Integer;
+begin
+  for i := 0 to High(bugs) do
+  begin
+    if SpriteValue(bugs[i], 'Alive') = 1 then
     begin
-      SpriteStartAnimation(game.buggy, 'splat');
-    end
-    else PlaySoundEffect('Hit');
+      for j := i + 1 to High(bugs) do
+      begin
+        if (SpriteValue(bugs[j], 'Alive') = 1) and SpriteCollision(bugs[i], bugs[j]) then 
+          CollideCircles(bugs[i], bugs[j]);
+      end;
+    end;
   end;
 end;
 
 procedure UpdateGame(var game: BuggyGameData);
+var
+  i, alive: Integer;
+  currentBug: Sprite;
 begin
-  UpdateSprite(game.buggy);
-  KeepOnScreen(game.buggy);
+  alive := 0;
 
-  if SpriteAnimationHasEnded(game.buggy) then
+  for i := 0 to High(game.bugs) do
   begin
-    PlaceBuggy(game.buggy);
+    currentBug := game.bugs[i];
+
+    UpdateSprite(currentBug);
+    KeepOnScreen(currentBug);
+
+    if SpriteValue(currentBug, 'Gone') = 0 then
+    begin
+      if SpriteValue(currentBug, 'Alive') = 0 then
+      begin
+        SpriteSetVelocity(currentBug, VectorMultiply(SpriteVelocity(game.bugs[i]), 0.98));
+
+        if SpriteAnimationHasEnded(game.bugs[i]) then
+        begin
+          SpriteSetValue(currentBug, 'Gone', 1);
+        end;
+      end;
+
+      alive += 1;
+    end; 
   end;
+
+  if alive = 0 then
+  begin
+    for i := 0 to High(game.bugs) do
+    begin
+      PlaceBuggy(game.bugs[i]);
+    end;
+
+    SetLength(game.bugs, Length(game.bugs) + 1);
+    CreateBug(game.bugs[High(game.bugs)]);
+  end;
+
+  // BounceBugs(game.bugs);
 end;
 
 procedure DrawGame(const game: BuggyGameData);
+var
+  i: Integer;
 begin
-  DrawSprite(game.buggy);
+  for i := 0 to High(game.bugs) do
+  begin
+    DrawSprite(game.bugs[i]);
+  end;
 end;
 
 
